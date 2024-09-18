5de355df1197fa89a9f05053ec8cca403da0fb9e
Venizio <gabinpincon@gmail.com>
2024-09-18 15:37:01 +0200

    [ADD]WalkAndIdleState

---
+++ Source/SmashUE/Private/Character/States/SmashCharacterStateWalk.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/States/SmashCharacterStateWalk.h"
+
+
+ESmashCharacterStateID USmashCharacterStateWalk::GetStateID()
+{
+	return ESmashCharacterStateID::Walk;
+}
---
+++ Source/SmashUE/Public/Character/States/SmashCharacterStateIdle.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/SmashCharacterState.h"
+#include "SmashCharacterStateIdle.generated.h"
+
+
+UCLASS(ClassGroup=(SmashCharacterState), meta=(BlueprintSpawnableComponent))
+class SMASHUE_API USmashCharacterStateIdle : public USmashCharacterState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	virtual ESmashCharacterStateID GetStateID() override;
+
+	
+};
